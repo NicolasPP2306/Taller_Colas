56c15f765d28a720eff718d4d04593b5a45aa63a
NicolasPP2306 <penaporrasnicolas@gmail.com>
2025-10-08 10:02:04 -0500

    Update StateBank.java

--- StateBank.java
+++ StateBank.java
@@ -6,12 +6,15 @@ public class StateBank {
     public static void main(String[] args) {
         Queue Register = new Queue();
         Queue Advisor = new Queue();
-        
         Queue Client = new Queue();
         Queue Preferencial = new Queue();
         Queue General = new Queue();
         
         int registers=3, advisors=2;
+        String option;
+        String menu[] = {"Assign turn", "Move to ticket office", "Size", "Delete", "Exit"};
+        
+        
     }
     
 }
