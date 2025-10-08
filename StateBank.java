69f19febec540bc2ad3c6d123fba25c948c4ba4d
NicolasPP2306 <penaporrasnicolas@gmail.com>
2025-10-08 10:18:39 -0500

    Update StateBank.java

--- StateBank.java
+++ StateBank.java
@@ -4,17 +4,26 @@ package taller_colas;
 public class StateBank {
 
     public static void main(String[] args) {
-        Queue Register = new Queue();
+        Queue CashRegister = new Queue();
         Queue Advisor = new Queue();
+        
         Queue Client = new Queue();
         Queue Preferencial = new Queue();
         Queue General = new Queue();
+        Queue Transactions  = new Queue();
         
         int registers=3, advisors=2;
         String option;
-        String menu[] = {"Assign turn", "Move to ticket office", "Size", "Delete", "Exit"};
+        String menu[] = {"Assign turn", "Pass to tramit", "Attend", "Statistics", "Exit"};
+        String statistics[] = {"Users per tramit", "Tramit made", "Total money", 
+                "Credit Mortgage value average", "Transactions per user"};
         
         
+    }
+    
+    public static String toString(Queue q) {
+        String text = "";
+        return text;
     }
     
 }
