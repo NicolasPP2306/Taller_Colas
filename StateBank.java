03a0159c1707d5ce87093cfadc491ea005ef0b24
NicolasPP2306 <penaporrasnicolas@gmail.com>
2025-10-08 09:42:29 -0500

    Update StateBank.java

--- StateBank.java
+++ StateBank.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package taller_colas;
 
-/**
- *
- * @author nicop
- */
 public class StateBank {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Queue Register = new Queue();
+        Queue Consultancy = new Queue();
+        Queue Client = new Queue();
+        Queue Preferencial = new Queue();
+        Queue General = new Queue();
+        
     }
     
 }
