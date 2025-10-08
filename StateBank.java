e92a1ff40ed3ee26c7557638eba809295e5aa796
NicolasPP2306 <penaporrasnicolas@gmail.com>
2025-10-08 09:58:13 -0500

    Update StateBank.java

--- StateBank.java
+++ StateBank.java
@@ -5,11 +5,13 @@ public class StateBank {
 
     public static void main(String[] args) {
         Queue Register = new Queue();
-        Queue Consultancy = new Queue();
+        Queue Advisor = new Queue();
+        
         Queue Client = new Queue();
         Queue Preferencial = new Queue();
         Queue General = new Queue();
         
+        int registers=3, advisors=2;
     }
     
 }
