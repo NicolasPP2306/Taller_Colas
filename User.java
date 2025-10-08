dd97c567bf1ee1d24226cc7660b002bc5dc5bbf4
NicolasPP2306 <penaporrasnicolas@gmail.com>
2025-10-08 09:36:57 -0500

    Update User.java

--- User.java
+++ User.java
@@ -5,4 +5,39 @@ public class User {
     private int id;
     private String process;
     private String type;
+
+    public User(int id, String process, String type) {
+        this.id = id;
+        this.process = process;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProcess() {
+        return process;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProcess(String process) {
+        this.process = process;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", process=" + process + ", type=" + type + '}';
+    }
 }
